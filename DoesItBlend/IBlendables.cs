d889b04dafcd8c0dc53af21ec3385502c00c8b94
Nathan Brown <njbrown@ksu.edu>
2019-09-20 14:29:31 -0500

    Blendable

---
+++ DoesItBlend/IBlendables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// Interface for items that can be blended
+    /// </summary>
+    interface IBlendables
+    {
+        /// <summary>
+        /// Blends the object
+        /// </summary>
+        /// <returns>The message regarding what got blended</returns>
+        string Blend();
+    }
+}
--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DoesItBlend
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendables> blendables = new List<IBlendables>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            for (int i =0; i < 10; i++)
+            {
+                blendables.Add(new Mango());
+            }
+
+            Banana b = new Banana();
+            Console.WriteLine(b.Blend());
+            blendables.Add(b);
+            string mess = "";
+            foreach( dynamic blendable in blendables)
+            {
+                mess += blendable.Blend(); 
+            }
+
+            
+            Console.WriteLine(mess);
         }
     }
 }
